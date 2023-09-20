ac90831c5ac93796517dfde53a80940b72e4bde6
elawy <m.nold@posteo.de>
2023-09-20 22:20:56 +0200

    change Labels

---
+++ Day_1/CalorieCounting.java
@@ -0,0 +1,59 @@
+package Day_1;
+
+import java.io.BufferedReader;
+import java.io.FileReader;
+import java.io.IOException;
+
+/**
+ * CalorieCounting
+ */
+public class CalorieCounting {
+
+
+    public static void main (String[] args){
+        getMostCalories();
+    }
+    
+    public static void getMostCalories(){
+
+        String filePath = "Day_1/input.txt";
+        int mostCalories = 0;
+        int secondMost = 0;
+        int thirdMost = 0;
+        int tempCalories = 0;
+
+        // read out of .txt file 
+        try(BufferedReader br = new BufferedReader(new FileReader(filePath))) {
+            String line;
+
+            while( ( line = br.readLine() )  != null){
+                if ( line.trim().isEmpty() ) {
+                    
+                    if (tempCalories > mostCalories){
+                        thirdMost = secondMost;
+                        secondMost = mostCalories;
+                        mostCalories = tempCalories;
+                    } 
+                    else if (tempCalories > secondMost){
+                        thirdMost = secondMost;
+                        secondMost = tempCalories;
+                    }    
+                    else if (tempCalories > thirdMost) thirdMost = tempCalories;
+                    tempCalories=0;
+                    
+                } else {
+                    tempCalories += Integer.parseInt( line.trim() );
+                }
+            }
+        } catch (IOException e) {
+            e.printStackTrace();
+        } catch (NumberFormatException e){
+            System.out.println("Ung;ltiges Format in der Datei");
+            e.printStackTrace();
+        }
+        
+        int sum = mostCalories+secondMost+thirdMost;
+        System.out.println("Calories: "+ sum);
+    }
+
+}
