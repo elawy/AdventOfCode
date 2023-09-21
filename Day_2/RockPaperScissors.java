aeafaddfbc732c3010448553a126421c5d1b12a9
elawy <m.nold@posteo.de>
2023-09-21 10:08:21 +0200

    added part 2 of day2

--- Day_2/RockPaperScissors.java
+++ Day_2/RockPaperScissors.java
@@ -7,7 +7,8 @@ import java.io.IOException;
 public class RockPaperScissors {
 
     static String filePath = "Day_2/input.txt";
-    static String[] handShapes = new String[2];
+    static String[] inputLine = new String[2];
+
 
     public static void main(String[] args){
         System.out.println(getTotalScore());
@@ -21,8 +22,9 @@ public class RockPaperScissors {
 
             while ( (line = br.readLine()) != null) {
                 // get seperated values
-                handShapes = line.split(" ");
-                totalScore += getRoundScore(handShapes);
+                inputLine = line.split(" ");
+                //totalScore += getRoundScore_1(inputLine);
+                totalScore += getRoundScore_2(inputLine);
 
             }
             return totalScore;
@@ -32,14 +34,58 @@ public class RockPaperScissors {
         return 0;
     }
 
-    public static int getRoundScore(String[] handShapes){
-        if (handShapes[1].equals("X")) return outCome(handShapes) + 1;
-        else if (handShapes[1].equals("Y")) return outCome(handShapes) + 2;
-        else if (handShapes[1].equals("Z")) return outCome(handShapes) + 3; 
+    public static int getShapeScoure(String shape){
+        if(shape.equals("A")) return 1;
+        else if(shape.equals("B")) return 2;
+        else if (shape.equals("C"))return 3;        
         else return 0;
     }
 
-    public static int outCome(String[] handShapes){
+    public static int getOutputScore(String output){
+        if(output.equals("X")) return 0;
+        else if(output.equals("Y")) return 3;
+        else if (output.equals("Z"))return 6;        
+        else return 0;
+    }
+
+    public static int calculateRoundScore(String shape, String output){
+        return getOutputScore(output) + getShapeScoure(shape);
+    }
+
+    public static int getRoundScore_2(String[] inputLine){
+        
+        switch (inputLine[0]) {
+            case "A":
+                switch (inputLine[1]) {
+                    case "X": return calculateRoundScore("C", inputLine[1]);
+                    case "Y": return calculateRoundScore("A", inputLine[1]);
+                    case "Z": return calculateRoundScore("B", inputLine[1]);
+                }
+            case "B":
+                switch (inputLine[1]) {
+                    case "X": return calculateRoundScore("A", inputLine[1]);
+                    case "Y": return calculateRoundScore("B", inputLine[1]);
+                    case "Z": return calculateRoundScore("C", inputLine[1]);
+                }
+            case "C":
+                switch (inputLine[1]) {
+                    case "X": return calculateRoundScore("B", inputLine[1]);
+                    case "Y": return calculateRoundScore("C", inputLine[1]);
+                    case "Z": return calculateRoundScore("A", inputLine[1]);
+                }
+            default:
+                 return 0;
+        }
+    }
+
+    public static int getRoundScore_1(String[] handShapes){
+        if (handShapes[1].equals("X")) return outCome_1(handShapes) + 1;
+        else if (handShapes[1].equals("Y")) return outCome_1(handShapes) + 2;
+        else if (handShapes[1].equals("Z")) return outCome_1(handShapes) + 3; 
+        else return 0;
+    }
+
+    public static int outCome_1(String[] handShapes){
         
         switch (handShapes[0]) {
             case "A":
@@ -49,8 +95,7 @@ public class RockPaperScissors {
                     case "Z": return 0;
                     default:
                         break;
-                }
-            
+                } 
             case "B":
                 switch (handShapes[1]) {
                     case "X": return 0;
@@ -59,7 +104,6 @@ public class RockPaperScissors {
                     default:
                         break;
                 }
-            
             case "C":
                 switch (handShapes[1]) {
                     case "X": return 6;
@@ -70,8 +114,6 @@ public class RockPaperScissors {
                 }
             default:
                 return 0;
-        }
-        
-        
+        } 
     }
 }
