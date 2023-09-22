0c36ae7d7de83187981852dee564702959a7054f
elawy <m.nold@posteo.de>
2023-09-22 13:50:58 +0200

    finished Day_3 - part2

--- Day_3/RucksackReorganization.java
+++ Day_3/RucksackReorganization.java
@@ -1,6 +1,7 @@
 package Day_3;
 
 import java.io.BufferedReader;
+import java.io.File;
 import java.io.FileReader;
 import java.io.IOException;
 
@@ -8,17 +9,63 @@ public class RucksackReorganization {
     static String filePath = "Day_3/input.txt";
 
     public static void main(String[] args){
-        System.out.println(getSumOfPriorities());
+        //System.out.println(getSumOfPriorities_part1());
+        System.out.println(getSumOfPriorities_part2());
     }
 
-    public static int getSumOfPriorities(){
+
+/*----------------------------for Part 1 and Part 2 ------------------------*/
+
+    public static int findPrioritieValue(char item){
+
+        String alphabetLow = "abcdefghijklmnopqrstuvwxyz";
+        String alphabetUpper = alphabetLow.toUpperCase();
+        String prioritie = alphabetLow + alphabetUpper;
+        for (int i = 0; i < prioritie.length(); i++) {
+            if (prioritie.charAt(i) == item) {
+                return i+1;
+            }
+        }
+        return 0;
+    }
+
+/*--------------------------------Part 2----------------------------------- */
+    public static int getSumOfPriorities_part2(){
+        try (BufferedReader br = new BufferedReader(new FileReader(filePath))){
+            String[] lines = new String[3];
+            int sumOfPriorities= 0;
+
+            while ( (lines[0]=br.readLine()) != null) {
+                for (int i = 1; i < 3; i++) lines[i] = br.readLine();
+                sumOfPriorities += findPrioritieValue(findItemType_part2(lines));
+            }
+            return sumOfPriorities;
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+        return 0;
+    }
+
+    
+    public static char findItemType_part2(String[] rucksack){
+        //find item that  exist in both elements
+        for (int i = 0; i < rucksack[0].length(); i++) {
+            if (rucksack[1].contains(rucksack[0].charAt(i)+"")
+                    && rucksack[2].contains(rucksack[0].charAt(i)+"") ) {
+                return rucksack[0].charAt(i);
+            }
+        }
+        return '0';
+    }
+/*---------------------------Part 1------------------------------------------------------ */
+    public static int getSumOfPriorities_part1(){
 
         try (BufferedReader br = new BufferedReader(new FileReader(filePath))){
             String line;
             int sumOfPriorities = 0;
 
             while ((line = br.readLine()) != null) {
-                char item = findItemType(divideInput(line));
+                char item = findItemType_part1(divideInput(line));
                 sumOfPriorities += findPrioritieValue(item);
             }
             return sumOfPriorities;       
@@ -35,7 +82,7 @@ public class RucksackReorganization {
         return rucksack;
     }
 
-    public static char findItemType(String[] rucksack){
+    public static char findItemType_part1(String[] rucksack){
         //find item that  exist in both elements
         for (int i = 0; i < rucksack[0].length(); i++) {
             if (rucksack[1].contains(rucksack[0].charAt(i)+"")) {
@@ -43,19 +90,6 @@ public class RucksackReorganization {
             }
         }
         return '0';
-    }
-
-    public static int findPrioritieValue(char item){
-
-        String alphabetLow = "abcdefghijklmnopqrstuvwxyz";
-        String alphabetUpper = alphabetLow.toUpperCase();
-        String prioritie = alphabetLow + alphabetUpper;
-        for (int i = 0; i < prioritie.length(); i++) {
-            if (prioritie.charAt(i) == item) {
-                return i+1;
-            }
-        }
-        return 0;
     }
 
 }
