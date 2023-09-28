c9a6549ad30023a038c2a2e1d80605282077df21
elawy <m.nold@posteo.de>
2023-09-28 11:37:35 +0200

    work on rekFunk() not finished yet

--- Day_7/NoSpaceLeftOnDevice.java
+++ Day_7/NoSpaceLeftOnDevice.java
@@ -3,10 +3,12 @@ package Day_7;
 import java.io.BufferedReader;
 import java.io.FileReader;
 import java.io.IOException;
+import java.util.HashMap;
 
 public class NoSpaceLeftOnDevice {
-    static String filePath = "Day_7/input.txt";
-    // map{key, value} => {folderName, folderSize}
+
+    static String filePath = "Day_7/inputTest.txt";
+    HashMap<String, Integer> foulders = new HashMap<String,Integer>();
     String line = "";
 
 
@@ -15,37 +17,83 @@ public class NoSpaceLeftOnDevice {
 
     public int getResult()throws IOException{
         BufferedReader br = new BufferedReader(new FileReader(filePath));
-        // add foulder -> "/" -> start
-        //
-   
-        // /.size= rekursionFunc(br, name, 0);
-        return 0;
+        line = br.readLine();
+        String[] lineArray = line.split(" ");
+        
+        int newSize = rekursionFunc(br, lineArray[2], 0);
+        foulders.replace(lineArray[2], foulders.get(lineArray[2]), newSize);
+        return findFoulder();
     }
 
-    public int rekursionFunc(BufferedReader br, String name, int size)throws IOException{
-        //add foulder to map
-       
+    public int rekursionFunc_x(BufferedReader br, String name, int size)throws IOException{
+
+        this.foulders.put(name, size);   //add foulder to map
+        int newSize;
+         // it is the line "ls" -> we don't need it, TODO could we jump over the line?
+        line = br.readLine();
+
         while ( ( line = br.readLine() ).charAt(0) != '$') {
-            /*
-            * if(number)
-            *   name.size += theNumber
-            */ 
+            String[] stringParts = line.split(" ");     //TODO line twice in this function
+            
+            if (isNumeric(stringParts[0])) {    //if the line shows a file to count
+                newSize = this.foulders.get(name) + Integer.parseInt(stringParts[0]);
+                this.foulders.replace(name, this.foulders.get(name), newSize);    // update size
+            }
         }
-        /*
-         * if(cd name)
-         *      name.size += rekursionFunc(name, 0);
-         * else if(cd ..)
-         *      return name.size;
-         */
-        return 0;
+        String[] stringParts = line.split(" ");
+
+        if ( (stringParts[1].equals("cd")) && (!stringParts[2].equals("..")) ) {   //it is another foulder 
+            newSize = this.foulders.get(name) + rekursionFunc(br, stringParts[2], 0);
+            this.foulders.replace(name, this.foulders.get(name), newSize);    // update size
+
+        }
+
+        return this.foulders.get(name);
     }
 
     public int findFoulder(){
         int sum = 0;
-        // loop throup map
-        //      if (size <= 100000) 
-        //          sum += size
+
+        for (int size  : this.foulders.values()) {
+            if (size <= 100000) {
+                sum += size;
+            }
+        }
+
         return sum;
     }
 
+    public static boolean isNumeric(String str) { 
+        try {  
+          Integer.parseInt(str);  
+          return true;
+        } catch(NumberFormatException e){  
+          return false;  
+        }  
+      }
+
+      public int rekursionFunc(BufferedReader br, String name, int size)throws IOException{
+
+        this.foulders.put(name, size);   //add foulder to map
+        int newSize;
+         // it is the line "ls" -> we don't need it, TODO could we jump over the line?
+        line = br.readLine();
+
+        while ( ( line = br.readLine() ) != null) {
+            String[] stringParts = line.split(" ");     //TODO line twice in this function
+            
+            if (isNumeric(stringParts[0])) {    //if the line shows a file to count
+                newSize = this.foulders.get(name) + Integer.parseInt(stringParts[0]);
+                this.foulders.replace(name, this.foulders.get(name), newSize);    // update size
+            } else if(( line = br.readLine() ).charAt(0) != '$'){
+                if ( (stringParts[1].equals("cd")) && (!stringParts[2].equals("..")) ) {   //it is another foulder 
+                    newSize = this.foulders.get(name) + rekursionFunc(br, stringParts[2], 0);
+                    this.foulders.replace(name, this.foulders.get(name), newSize);    // update size
+                }else{
+                    return this.foulders.get(name);
+                }
+            }
+        }  
+        return 0;    
+    }
 }
--- Day_7/main.java
+++ Day_7/main.java
@@ -1,10 +1,14 @@
 package Day_7;
 
+import java.io.IOException;
 
 public class main {
     public static void main(String[] args){
         NoSpaceLeftOnDevice obj = new NoSpaceLeftOnDevice();
-        System.out.println(obj.getResult());
+        try {
+            System.out.println(obj.getResult());
+        } catch (IOException e) {
+             e.printStackTrace();
+        }
     }
-
 }
