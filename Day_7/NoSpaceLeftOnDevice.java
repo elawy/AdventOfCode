db05924aae487935c3b2c06398bea78fcd21508d
elawy <m.nold@posteo.de>
2023-09-28 12:54:00 +0200

    inputTest works, input not yet

--- Day_7/NoSpaceLeftOnDevice.java
+++ Day_7/NoSpaceLeftOnDevice.java
@@ -7,7 +7,7 @@ import java.util.HashMap;
 
 public class NoSpaceLeftOnDevice {
 
-    static String filePath = "Day_7/inputTest.txt";
+    static String filePath = "Day_7/input.txt";
     HashMap<String, Integer> foulders = new HashMap<String,Integer>();
     String line = "";
 
@@ -25,41 +25,38 @@ public class NoSpaceLeftOnDevice {
         return findFoulder();
     }
 
-    public int rekursionFunc_x(BufferedReader br, String name, int size)throws IOException{
+    public int rekursionFunc(BufferedReader br, String name, int size)throws IOException{
 
         this.foulders.put(name, size);   //add foulder to map
         int newSize;
          // it is the line "ls" -> we don't need it, TODO could we jump over the line?
         line = br.readLine();
 
-        while ( ( line = br.readLine() ).charAt(0) != '$') {
-            String[] stringParts = line.split(" ");     //TODO line twice in this function
+        while ( ( line = br.readLine() ) != null) {
+            String[] stringParts = line.split(" ");
             
             if (isNumeric(stringParts[0])) {    //if the line shows a file to count
                 newSize = this.foulders.get(name) + Integer.parseInt(stringParts[0]);
                 this.foulders.replace(name, this.foulders.get(name), newSize);    // update size
+            } else if( stringParts[0].equals("$")){
+                if ( (stringParts[1].equals("cd")) && (!stringParts[2].equals("..")) ) {   //it is another foulder 
+                    newSize = this.foulders.get(name) + rekursionFunc(br, stringParts[2], 0);
+                    this.foulders.replace(name, this.foulders.get(name), newSize);    // update size
+                }else{
+                    return this.foulders.get(name);
+                }
             }
-        }
-        String[] stringParts = line.split(" ");
-
-        if ( (stringParts[1].equals("cd")) && (!stringParts[2].equals("..")) ) {   //it is another foulder 
-            newSize = this.foulders.get(name) + rekursionFunc(br, stringParts[2], 0);
-            this.foulders.replace(name, this.foulders.get(name), newSize);    // update size
-
-        }
-
+        }  
         return this.foulders.get(name);
-    }
+    } 
+
 
     public int findFoulder(){
         int sum = 0;
 
         for (int size  : this.foulders.values()) {
-            if (size <= 100000) {
-                sum += size;
-            }
+            if (size <= 100000) sum += size;
         }
-
         return sum;
     }
 
@@ -70,30 +67,5 @@ public class NoSpaceLeftOnDevice {
         } catch(NumberFormatException e){  
           return false;  
         }  
-      }
-
-      public int rekursionFunc(BufferedReader br, String name, int size)throws IOException{
-
-        this.foulders.put(name, size);   //add foulder to map
-        int newSize;
-         // it is the line "ls" -> we don't need it, TODO could we jump over the line?
-        line = br.readLine();
-
-        while ( ( line = br.readLine() ) != null) {
-            String[] stringParts = line.split(" ");     //TODO line twice in this function
-            
-            if (isNumeric(stringParts[0])) {    //if the line shows a file to count
-                newSize = this.foulders.get(name) + Integer.parseInt(stringParts[0]);
-                this.foulders.replace(name, this.foulders.get(name), newSize);    // update size
-            } else if(( line = br.readLine() ).charAt(0) != '$'){
-                if ( (stringParts[1].equals("cd")) && (!stringParts[2].equals("..")) ) {   //it is another foulder 
-                    newSize = this.foulders.get(name) + rekursionFunc(br, stringParts[2], 0);
-                    this.foulders.replace(name, this.foulders.get(name), newSize);    // update size
-                }else{
-                    return this.foulders.get(name);
-                }
-            }
-        }  
-        return 0;    
     }
 }
