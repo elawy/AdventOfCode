60d77a0b17abb31d1aad266885b5dba04b1222d4
elawy <m.nold@posteo.de>
2023-09-29 14:38:04 +0200

    structure

--- Day_8/MainClass.java
+++ Day_8/MainClass.java
@@ -1,14 +1,10 @@
 package Day_8;
 
-import java.io.IOException;
 
 public class MainClass {
     public static void main(String[] args){
-        try {
-            TreetopTreeHouse obj = new TreetopTreeHouse();
-            System.out.println(obj.getResult());
-        } catch (IOException e) {
-             e.printStackTrace();
-        }
+        
+        TreetopTreeHouse obj = new TreetopTreeHouse();
+        System.out.println(obj.getResult());
     }
 }
--- Day_8/TreetopTreeHouse.java
+++ Day_8/TreetopTreeHouse.java
@@ -3,14 +3,40 @@ package Day_8;
 import java.io.BufferedReader;
 import java.io.FileReader;
 import java.io.IOException;
+import java.util.ArrayList;
 
 public class TreetopTreeHouse {
     static String filePath = "Day_8/input.txt";
+    private ArrayList<ArrayList<Integer>> treeMap;
     
-    public int getResult()throws IOException{
-        BufferedReader br = new BufferedReader(new FileReader(filePath));
-        String line = "";
-        line=br.readLine();
-        return 0;
+    public int getResult(){
+        createMap();
+        return countEdgeTrees() + countInteriorVisibles();
+    }
+
+    public void createMap(){
+        try {
+            BufferedReader br = new BufferedReader(new FileReader(filePath));
+            String line = "";
+
+        while ( (line = br.readLine().trim()) != null ) {
+            for (int i = 0; i < line.length(); i++) {
+                // TODO
+            }
+        }
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+    }
+
+    public int countInteriorVisibles(){
+        int interiorVisible = 0;
+        //TODO
+        return interiorVisible;
+    }
+    public int countEdgeTrees(){
+        int edgeTrees = 0;
+        //TODO
+        return edgeTrees;
     }
 }
