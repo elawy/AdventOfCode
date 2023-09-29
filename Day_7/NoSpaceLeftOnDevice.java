8476970f25454cbc28128040c3732629896d6999
elawy <m.nold@posteo.de>
2023-09-29 10:44:40 +0200

    Day_7 finished part 2

--- Day_7/NoSpaceLeftOnDevice.java
+++ Day_7/NoSpaceLeftOnDevice.java
@@ -5,12 +5,13 @@ import java.io.FileReader;
 import java.io.IOException;
 import java.util.HashMap;
 
+
 public class NoSpaceLeftOnDevice {
 
     static String filePath = "Day_7/input.txt";
-    HashMap<String, Integer> foulders = new HashMap<String,Integer>();
+    HashMap<String, Integer> directory = new HashMap<String,Integer>();
     String line = "";
-
+    int sameName = 0;
 
     public NoSpaceLeftOnDevice(){
     }
@@ -22,46 +23,51 @@ public class NoSpaceLeftOnDevice {
         String[] lineArray = line.split(" ");
         
         int newSize = rekursionFunc(br, lineArray[2], 0);
-        foulders.replace(lineArray[2], foulders.get(lineArray[2]), newSize);
-        return findFoulder();
+        directory.replace(lineArray[2], directory.get(lineArray[2]), newSize);
+        
+        // -----------------Part_1---------------
+        //return findFoulder();
+        // -----------------Part_2---------------
+        return directoryToDelete();
     }
 
 
 
     public int rekursionFunc(BufferedReader br, String name, int size)throws IOException{
 
-        if (foulders.containsKey(name))name += "_sub";
-        this.foulders.put(name, size);   //add foulder to map
+        if (directory.containsKey(name)){
+            sameName++;
+            name += Integer.toString(sameName);
+        }
+        this.directory.put(name, size);  //add foulder to map
+
+
         int newSize;
-         // it is the line "ls" -> we don't need it, TODO could we jump over the line?
-        line = br.readLine();
+        br.readLine();// it is the line "ls" -> we don't need it
 
         while ( ( line = br.readLine() ) != null) {
             String[] stringParts = line.split(" ");
             
             if (isNumeric(stringParts[0])) {    //if the line shows a file to count
-                newSize = this.foulders.get(name) + Integer.parseInt(stringParts[0]);
-                this.foulders.replace(name, this.foulders.get(name), newSize);    // update size
+                newSize = this.directory.get(name) + Integer.parseInt(stringParts[0]);
+                this.directory.replace(name, this.directory.get(name), newSize);    // update size
             } else if( stringParts[0].equals("$")){
-                if ( (stringParts[1].equals("cd")) && (!stringParts[2].equals("..")) ) {   //it is another foulder 
-                    newSize = this.foulders.get(name) + rekursionFunc(br, stringParts[2], 0);
-                    this.foulders.replace(name, this.foulders.get(name), newSize);    // update size
+                if ( (stringParts[1].equals("cd")) && (!stringParts[2].equals("..")) ) {  //it is another  sub-directory 
+                    newSize = this.directory.get(name) + rekursionFunc(br, stringParts[2], 0);
+                    this.directory.replace(name, this.directory.get(name), newSize);    // update size
                 }else{
-                    return this.foulders.get(name);
+                    return this.directory.get(name);
                 }
             }
         }  
-        return this.foulders.get(name);
+        return this.directory.get(name);
     } 
 
 
     public int findFoulder(){
         int sum = 0;
-
-        for (int size  : this.foulders.values()) {
-            if (size <= 100000){
-                sum += size;
-            }
+        for (HashMap.Entry<String, Integer> set : directory.entrySet()){
+            if (set.getValue() <= 100000) sum += set.getValue();
         }
         return sum;
     }
@@ -74,5 +80,24 @@ public class NoSpaceLeftOnDevice {
         } catch(NumberFormatException e){  
           return false;  
         }  
+    }
+
+
+    public int directoryToDelete(){
+        int dirToDelete = directory.get("/");
+
+        for (Integer value : directory.values()) {
+            if (value >= calcNeededSpace()) {
+                if (dirToDelete > value) dirToDelete = value;
+            }
+        }
+        return dirToDelete;
+    }
+
+    public int calcNeededSpace(){
+        int diskSpace = 70000000;
+        int neededSpace = 30000000;
+
+        return directory.get("/") - (diskSpace - neededSpace);
     }
 }
