27bb8fbc37b1439d54dff692540eeea5083978f7
elawy <m.nold@posteo.de>
2023-10-06 18:51:03 +0200

    create ArrayList from ArrayList

--- Day_8/TreetopTreeHouse.java
+++ Day_8/TreetopTreeHouse.java
@@ -7,23 +7,26 @@ import java.util.ArrayList;
 
 public class TreetopTreeHouse {
     static String filePath = "Day_8/input.txt";
-    private ArrayList<ArrayList<Integer>> treeMap;
+    private ArrayList<ArrayList<Integer>> treeMap = new ArrayList<ArrayList<Integer>>();
     
     public int getResult(){
         createMap();
+        //toPrint();
         return countEdgeTrees() + countInteriorVisibles();
     }
 
     public void createMap(){
-        try {
-            BufferedReader br = new BufferedReader(new FileReader(filePath));
-            String line = "";
 
-        while ( (line = br.readLine().trim()) != null ) {
-            for (int i = 0; i < line.length(); i++) {
-                // TODO
+        ArrayList<Integer> temp;
+        try (BufferedReader br = new BufferedReader(new FileReader(filePath))) {
+            String line = "";
+            while ((line = br.readLine()) != null ) {
+                temp = new ArrayList<Integer>();
+                for (int j = 0; j < line.length(); j++) {
+                    temp.add( Character.getNumericValue(line.charAt(j)) );
+                }
+                this.treeMap.add(temp);
             }
-        }
         } catch (IOException e) {
             e.printStackTrace();
         }
@@ -36,7 +39,18 @@ public class TreetopTreeHouse {
     }
     public int countEdgeTrees(){
         int edgeTrees = 0;
-        //TODO
+        int rows = this.treeMap.size();
+        int colums = this.treeMap.get(0).size();
+        edgeTrees = 2*rows + 2* (colums -2);
         return edgeTrees;
+    }
+
+    public void toPrint(){
+        for (int i = 0; i < this.treeMap.size(); i++) {
+            for (int j = 0; j < this.treeMap.get(i).size(); j++) {
+                System.out.print(this.treeMap.get(i).get(j));
+            }
+            System.out.print("\n");
+        }
     }
 }
